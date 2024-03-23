09870d5c3944495ce29ba86a6976843040c852d3
lwuces <Thanawat.mixx@gmail.com>
2024-03-23 21:34:17 +0700

    23/03/2024 Manage fix

--- src/main/java/com/example/demo/ui/ManagePage.java
+++ src/main/java/com/example/demo/ui/ManagePage.java
@@ -4,7 +4,6 @@ import org.vaadin.crudui.crud.impl.GridCrud;
 
 import com.example.demo.backend.Students;
 import com.example.demo.backend.Service.StudentsService;
-import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
@@ -13,21 +12,12 @@ import lombok.var;
 @Route(value = "Manage-page", layout = MainView.class)
 public class ManagePage extends VerticalLayout{
 
-    private StudentsService service ;
-
-    public ManagePage() {
-        initcompoment();
-        setDisplay();
-    }
-
-    private void initcompoment() {
+    public ManagePage(StudentsService service) {
         var crud = new GridCrud<>(Students.class, service);
         crud.getGrid().setColumns("studentid","firstname", "lastname", "email", "birth");
         crud.getCrudFormFactory().setVisibleProperties("studentid","firstname", "lastname", "email", "birth");
-        add(
-            new H1("Student<>Mangement"),
-            crud
-        );
+        add(crud);
+        setDisplay();
     }
 
     private void setDisplay() {
